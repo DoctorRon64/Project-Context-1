5fdd18f4a360e42e632ee3dcee7354e8e363a057
GooForSupper <llvandersloot@gmail.com>
2023-03-12 18:17:07 +0100

    Deadline script
    'Deadline' script die een timer toevoegt en de eerstvolgende scene laad als deze 0 bereikt. Dit script staat op een object genaamd 'Deadline Manager'. Ook word de timer afgebeeld op het scherm via de 'Deadline Canvas'.

---
+++ projectcontexteen/Assets/MadeByLucas/Deadline.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Deadline : MonoBehaviour
+{
+    public float currentTime;
+    public float maxTime = 100;
+
+    public TextMeshProUGUI deadlineTimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DepleteTime();
+        CheckTime();
+        DisplayTimeOnScreen();
+    }
+
+    void DepleteTime() // Decrease time over... time!
+    {
+        currentTime -= Time.deltaTime;
+    }
+
+    void CheckTime() // Check if time is over
+    {
+        if (currentTime <= 0)
+        {
+            DeadlineHit();
+        }
+    }
+
+    void DeadlineHit() // Call function when time has hit 0
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void DisplayTimeOnScreen() // Show the current time on the screen
+    {
+        deadlineTimerText.text = currentTime.ToString("F1");
+    }
+
+
+    public void DecreaseTimeBy(float amount) // Decrease the time by a specific amount instantly
+    {
+        currentTime -= amount;
+    }
+}

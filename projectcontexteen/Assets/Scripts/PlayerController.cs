a85d20682a12da79aee4ddd19f9c2a0e710de505
DrRon <piepmail@lefthandmedia.com>
2023-03-13 02:56:50 +0100

    New Input Systtem

--- projectcontexteen/Assets/Scripts/PlayerController.cs
+++ projectcontexteen/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,53 +9,40 @@ public class PlayerController : MonoBehaviour
     public float JumpHeight;
     public float OverlapSphere;
 
-    public float horizontalInput;
-
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Rigidbody2D rb2d;
     [SerializeField] private LayerMask layerMask;
 
-    public KeyCode LeftInput;
-    public KeyCode RightInput;
-    public KeyCode UpInput;
+    private Vector2 movementInput;
+    private bool jumped = false;
 
-    void Start()
+    void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
+        rb2d.velocity = new Vector2(movementInput.x * Speed, rb2d.velocity.y);
 
-        PlayerInput();
-    }
-
-	private void PlayerInput()
-	{
-        //horizontalInput = Input.GetAxisRaw("Horizontal");
-        
-
-
-        /*if (Input.GetKeyDown(LeftInput))
-		{
-            horizontalInput = 1;
-		}
-
-
-        if (Input.GetKeyDown(RightInput))
-		{
-            horizontalInput = -1;
-		}*/
-
-        if (Input.GetKey(UpInput) && IsGrounded())
+        if (jumped && IsGrounded())
         {
             rb2d.AddForce(Vector2.up * JumpHeight, ForceMode2D.Impulse);
         }
-
-        rb2d.velocity = new Vector2(horizontalInput * Speed, rb2d.velocity.y);
     }
 
-    private bool IsGrounded()
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+	{
+        jumped = context.ReadValue<bool>();
+        jumped = context.action.triggered;
+	}
+
+	private bool IsGrounded()
 	{
         return Physics2D.OverlapCircle(groundCheck.position, OverlapSphere, layerMask);
 	}

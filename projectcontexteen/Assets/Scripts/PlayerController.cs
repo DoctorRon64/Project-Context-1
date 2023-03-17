6eed4d6722faf87b7a3e7bae0547aff707c5379e
DrRon <piepmail@lefthandmedia.com>
2023-03-17 03:00:04 +0100

    WAAROM WERKEN DIE FACKING ANIMATIES NIET

--- projectcontexteen/Assets/Scripts/PlayerController.cs
+++ projectcontexteen/Assets/Scripts/PlayerController.cs
@@ -14,41 +14,96 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb2d;
     [SerializeField] private LayerMask layerMask;
 
+    public AnimationCurve layerWeightCurve;
     public float horizontalInput;
     public string MoveInput;
 	public KeyCode UpInput;
     public bool jumped = false;
+    public bool jumpeded = false;
 
-	void Awake()
+    void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
 	private void Update()
 	{
-        horizontalInput = Input.GetAxisRaw(MoveInput);
-        if (Input.GetKey(UpInput))
-		{
-            jumped = true;
-		} else
-		{
-            jumped = false;
-		}
+        Walking();
+        Idle();
+        Jumping();
+
+        anim.SetInteger("Casino", 1);
+        anim.SetInteger("Casino", 0);
     }
 
-	void FixedUpdate()
-    {
+    void FixedUpdate()
+	{
+		rb2d.velocity = new Vector2(horizontalInput * Speed, rb2d.velocity.y);
         if (jumped && IsGrounded())
-		{
+        {
             rb2d.AddForce(Vector2.up * JumpHeight, ForceMode2D.Impulse);
+            AnimationControls();
         }
+        jumpeded = IsGrounded();
+    }
 
-        rb2d.velocity = new Vector2(horizontalInput * Speed, rb2d.velocity.y);
+    void Idle()
+	{
+        if (jumped == false && horizontalInput == 0)
+        {
+            anim.SetLayerWeight(0, 0);
+        }
+    }
+
+    void Walking()
+	{
+        horizontalInput = Input.GetAxisRaw(MoveInput); 
+
+        if (horizontalInput > 0)
+        {
+            anim.SetLayerWeight(1, 0);
+            anim.SetBool("Walking", true);
+        } 
+        else if (horizontalInput < 0)
+		{
+            anim.SetLayerWeight(0, 0);
+            anim.SetBool("Walking", false);
+        }
+    }
+
+    void Jumping()
+	{
+        if (Input.GetKey(UpInput)) { 
+            jumped = true;
+        } else { 
+            jumped = false;
+        }
     }
 
 	private bool IsGrounded()
 	{
         return Physics2D.OverlapCircle(groundCheck.position, OverlapSphere, layerMask);
+	}
+
+    private void AnimationControls()
+	{
+        if (rb2d.velocity.y < 0)
+		{
+            float curveWeight = rb2d.velocity.y;
+            curveWeight = Mathf.Abs(curveWeight);
+            curveWeight /= 40;
+            curveWeight = Mathf.Clamp(curveWeight, 0, 1);
+            anim.SetLayerWeight(2, layerWeightCurve.Evaluate(curveWeight));
+
+            anim.SetBool("GoingUp", false);
+            anim.SetBool("Airborne", true);
+        }
+        else
+		{
+            anim.SetLayerWeight(2, 0);
+            anim.SetBool("GoingUp", true);
+            anim.SetBool("Airborne", false);
+        }
 	}
 }
 

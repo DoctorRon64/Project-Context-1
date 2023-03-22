05dcd59b8330c3463f6423413a6197a1a6829c12
GooForSupper <llvandersloot@gmail.com>
2023-03-22 23:15:03 +0100

    Time's Up! popup + Slider fix
    Een geanimeerde popup voor als de tijd voorbij is. Ook het probleem met de Deadline Canvas bar gefixed waar deze nooit helemaal vol of leeg kon zijn.

--- projectcontexteen/Assets/Scripts/RestartGame.cs
+++ projectcontexteen/Assets/Scripts/RestartGame.cs
@@ -14,7 +14,7 @@ public class RestartGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.anyKey)
         {
             Restart();
         }

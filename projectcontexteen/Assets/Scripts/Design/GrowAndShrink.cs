17b2c0ecf67407233cc628c61b9b4c5479869444
Rick-van-Leeuwen <124429419+Rick-van-Leeuwen@users.noreply.github.com>
2023-03-24 20:06:19 +0100

    Grow and shrink bewerkt. Tutorial af
    Grow and shrink variabelen bewerkt. Tutorial is af buiten het functioneel maken van de deur (Is volgens mij gewoon de scene order) en de tutorial prompts voor de controls

--- projectcontexteen/Assets/Scripts/Design/GrowAndShrink.cs
+++ projectcontexteen/Assets/Scripts/Design/GrowAndShrink.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GrowAndShrink : MonoBehaviour
 {
     public float scaleFactor = 0.1f;
-    public float maxScale = 12f;
+    public float maxScale = 30f;
     public float minScale = 10f;
     public float scaleSpeed = 3f;
     public bool STOP = false;
